30066807ec58b5bb08cbb01760f2b89689e707b1
hx <www.1567891@qq.com>
2018-05-07 19:22:55 +0800

    ..

--- main.js
+++ main.js
@@ -1,69 +1,89 @@
-
 class Rate {
-  constructor(id, type = 'pie') {
-    this.type = type;
+  constructor(id, options) {
+    // type： "pie" || "linePress" || "sandGlass"
+    this.type = options.type || "pie";
     this.container = document.getElementById(id);
-    this.rotate = this.container.getAttribute('data-rate');
+    this.rotate = options.rotate || this.container.getAttribute("data-rate");
+    this.initRotate = 0;
+    this.color = options.color || "green";
   }
   render() {
-    if (this.container && this.rotate) {
-      switch (this.type) {
-        case 'pie':
-          this.renderPie();
-          break;
-        case 'linePress':
-          this.renderLine();
-          break;
-        case 'sandGlass':
-          this.renderSandGlass();
-          break;
-        default:
-          break;
+    const s = setInterval(() => {
+      this.initRotate++;
+      if (this.initRotate >= this.rotate) {
+        clearInterval(s);
       }
-    }
+      if (this.container) {
+        switch (this.type) {
+          case "pie":
+            this.renderPie(this.initRotate);
+            break;
+          case "linePress":
+            this.renderLine(this.initRotate);
+            break;
+          case "sandGlass":
+            this.renderSandGlass(this.initRotate);
+            break;
+          default:
+            break;
+        }
+      }
+    }, 16);
   }
-  renderPie() {
-    const content = `<div></div><span>${this.rotate}%</span>`;
+
+  renderPie(rotate) {
+    const content = `<div></div><span>${rotate}%</span>`;
     this.container.innerHTML = content;
-    this.container.className = 'pie';
+    this.container.className = "pie";
     const rateDive = this.container.children[0];
-    if (this.rotate < 50) {
-      rateDive.style.transform = `rotate(${this.rotate/100}turn)`;
+
+    if (rotate < 50) {
+      rateDive.style.transform = `rotate(${rotate / 100}turn)`;
     } else {
-      this.container.className = 'pie pieMore50';
-      rateDive.id = 'roaterMore50';
-      const rotate = parseInt(this.rotate) - 100;
-      rateDive.style.transform = `rotate(${rotate/100}turn)`;
+      this.container.className = "pie pieMore50";
+      rateDive.id = "roaterMore50";
+      let rotate = parseInt(rotate) - 100;
+      rateDive.style.transform = `rotate(${rotate / 100}turn)`;
     }
   }
-  renderLine() {
-    const content = `<div><span>${this.rotate}%</span></div>`;
+  renderLine(rotate) {
+    const content = `<div><span>${rotate}%</span></div>`;
     this.container.innerHTML = content;
-    this.container.className = 'progress';
+    this.container.className = "progress";
     const rateDive = this.container.children[0];
     const showData = rateDive.children[0];
-    rateDive.style.background =
-      `linear-gradient(to right, greenyellow ${this.rotate}%, gray ${this.rotate}%)`;
+    rateDive.style.background = `linear-gradient(to right, greenyellow ${rotate}%, gray ${rotate}%)`;
   }
-  renderSandGlass() {
+  renderSandGlass(rotate) {
     const content = `<div></div>`;
     this.container.innerHTML = content;
-    const out = (1 - (this.rotate / 100)) * 2500;
+    const out = (1 - rotate / 100) * 2500;
     const restLen = Math.sqrt(out) * 2;
     const outLen = 100 - restLen;
     const bottom = restLen + 100;
-    this.container.className = 'sandGlass';
-    this.container.style.background =
-      `linear-gradient(to bottom, gray ${outLen}px, greenyellow ${outLen}px, gray ${bottom}px,
+    this.container.className = "sandGlass";
+    this.container.style.background = `linear-gradient(to bottom, gray ${outLen}px, greenyellow ${outLen}px, gray ${bottom}px,
         greenyellow ${bottom}px)`;
-      // 'linear-gradient(to bottom, gray  ' +
-      // outLen + 'px, greenyellow  ' + outLen + 'px, greenyellow 100px, gray 100px, gray ' + bottom + 'px, greenyellow ' + bottom + 'px)';
+    // 'linear-gradient(to bottom, gray  ' +
+    // outLen + 'px, greenyellow  ' + outLen + 'px, greenyellow 100px, gray 100px, gray ' + bottom + 'px, greenyellow ' + bottom + 'px)';
   }
 }
 
-const rate = new Rate('pie__plugin', 'pie');
+const rate = new Rate("pie__plugin",  {
+  type: "pie",
+  rotate: 40,
+  color: "red"
+});
 rate.render();
-const linePress = new Rate('progress__plugin', 'linePress');
+const linePress = new Rate("progress__plugin", {
+  type: "linePress",
+  rotate: 60,
+  color: "red"
+});
 linePress.render();
-const sandGlass = new Rate('sandGlass__plugin', 'sandGlass');
+const sandGlass = new Rate("sandGlass__plugin",  {
+  type: "sandGlass",
+  rotate: 80,
+  color: "red"
+});
 sandGlass.render();
